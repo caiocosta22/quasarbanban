effa1b4f0aba0cf91e42ee24780e3616219da573
Caio Costa <caiocosta22@gmail.com>
2023-10-25 18:31:01 -0300

    ajustes navegacao

--- src/components/cabecalho.vue
+++ src/components/cabecalho.vue
@@ -12,6 +12,28 @@ const router = useRouter();
 const pesquisa = ref("");
 const prompt = ref(false);
 const drawer = ref(false);
+const categoriasCarousel = ref([
+  {
+    descricao: "CARTEIRAS",
+    fotoUrl: "https://cs210033fff90d2f7ac.blob.core.windows.net/banbancalcados/categoria-autorelacionada-servico/3368imagem2023-09-28T11:56:50.209.png",
+    googleCategoriaProduto: "2668 - VestuÃ¡rio e acessÃ³rios > Bolsas, carteiras e estojos > Carteiras e clipes de dinheiro",
+    id: 151,
+    menuEcommerce: true,
+    ordem: 3,
+    slug: "carteiras",
+    subCategoria: []
+  },
+  {
+    descricao: "BOLSAS",
+    fotoUrl: "https://cs210033fff90d2f7ac.blob.core.windows.net/banbancalcados/categoria-autorelacionada-servico/2913imagem2023-09-28T11:53:44.207.png",
+    googleCategoriaProduto: "106 - Malas e bolsas > Bolsas a tiracolo",
+    id: 159,
+    menuEcommerce: true,
+    ordem: 11,
+    slug: "bolsas",
+    subCategoria: []
+  }
+]);
 
 const quantidadeCarrinho = ref($q.localStorage.getItem("quantidadeCarrinho") || 0);
 // const cartId = $q.localStorage.getItem("cartIdBackend");
@@ -24,6 +46,22 @@ const props = defineProps({
   }
 });
 
+async function searchCategories () {
+  try {
+    const categorias = await axios.get("https://banbancalcados.elevarcommerceapi.com.br/HandoverMetasWS/webapi/handover/portal/ecommerce/categoriaAutoRelacionada/getAllCategorias").then(e => e.data);
+    const categoriascomFoto = categorias.filter(categoria => categoria.fotoUrl);
+    if (categorias.length) categoriasCarousel.value = categoriascomFoto;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function openCategoryPage (categoria) {
+  if (categoria.id) {
+    const url = "/categorias/" + categoria.id;
+    router.push(url);
+  }
+}
 function redirectToSearchPage () {
   if (pesquisa.value) {
     const url = "/pesquisa/" + pesquisa.value;
@@ -38,6 +76,7 @@ setInterval(async () => {
 onBeforeMount(async () => {
   await Promise.all([
     // getCartItems()
+    searchCategories()
   ]);
 });
 </script>
@@ -114,7 +153,7 @@ div.container
   )
     q-scroll-area(class="fit")
       q-list
-        q-item(
+        q-item#minibusca(
           clickable
           v-ripple
           style="color:black"
@@ -232,12 +271,30 @@ div>a {
   line-height: normal;
   padding-left: 3px
 }
-@media screen and (min-width: 1150px) {
+@media screen and (min-width: 1024px) {
   .multimenu{
     display:none
   }
   .botaomenu{
     display:none
+  }
+}
+@media screen and (max-width: 1024px) {
+  .texto{
+    display:none
+  }
+}
+@media screen and (min-width: 768px) and (max-width: 1024px) {
+  #minibusca {
+    display: none;
+  }
+}
+@media screen and (max-width: 768px) {
+  .texto{
+    display:none
+  }
+  .containerinput {
+    display: none;
   }
 }
 </style>
