73d9ff3465564b3634247e307ac233cb326b7493
caiocosta22 <caiocosta22@gmail.com>
2023-11-29 17:17:17 -0300

    adicionado teste de cookies

---
+++ src/components/cookies.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted } from "vue";
+import { useQuasar } from "quasar";
+const $q = useQuasar();
+function positioned () {
+  $q.dialog({
+    title: "Alerta de Cookies.",
+    message: "Ao clicar em Ok, você concorda com nossos Cookies e políticas de privacidade.",
+    ok: {
+      push: true
+    },
+    cancel: {
+      push: true,
+      color: "negative"
+    },
+    persistent: true,
+    position: "bottom"
+  });
+}
+onMounted(() => {
+  positioned();
+});
+</script>
--- src/layouts/padrao.vue
+++ src/layouts/padrao.vue
@@ -1,3 +1,54 @@
+<script setup>
+import { onMounted } from "vue";
+import { useQuasar } from "quasar";
+import cabecalho from "../components/cabecalho.vue";
+import navegacao from "src/components/navegacao.vue";
+import banneranuncio from "../components/banners/banneranuncio.vue";
+import rodape from "src/components/rodape.vue";
+
+const $q = useQuasar();
+
+const options = {
+  expires: "365d"
+};
+
+const politicas = $q.cookies.has("politicas");
+
+function setcookies () {
+  $q.cookies.set("politicas", true, options);
+}
+
+function showCookieDialog () {
+  if (!politicas) {
+    $q.dialog({
+      message: "Políticas de Privacidade: Ao clicar em Ok, você concorda com o armazenamento de cookies no seu dispositivo para melhorar a navegação no site, analisar a utilização do site e ajudar nas nossas iniciativas de marketing.",
+      ok: {
+        push: true,
+        color: "black",
+        padding: "8px 60px",
+        onclick: setcookies
+      },
+      persistent: true,
+      position: "bottom",
+      fullWidth: true,
+      seamless: true,
+      style: {
+        background: "rgba(255,255,255,0.8)",
+        fontWeight: "600",
+        fontSize: "16px",
+        textAlign: "left",
+        marginBlock: "center"
+      },
+      square: true
+    });
+  }
+}
+
+onMounted(() => {
+  showCookieDialog();
+});
+</script>
+
 <template lang="pug">
 q-layout(view="lHh Lpr lFf")
   q-header
@@ -7,10 +58,3 @@ q-layout(view="lHh Lpr lFf")
   router-view
   rodape
 </template>
-
-<script setup>
-import cabecalho from "../components/cabecalho.vue";
-import navegacao from "src/components/navegacao.vue";
-import banneranuncio from "../components/banners/banneranuncio.vue";
-import rodape from "src/components/rodape.vue";
-</script>
