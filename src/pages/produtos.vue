49e243c5cb5e99a81b80347c36c8dc26fc1d63c6
caiocosta22 <caiocosta22@gmail.com>
2023-11-30 16:09:09 -0300

    redirecionamento para produtos

--- src/pages/produtos.vue
+++ src/pages/produtos.vue
@@ -1,44 +1,46 @@
 <script setup>
-import { ref } from "vue";
-// import { useQuasar } from "quasar";
-// import { useRoute, useRouter } from "vue-router";
-// import axios from "axios";
-// const route = useRoute();
-// const router = useRouter();
-// const $q = useQuasar();
-const itsLoading = ref(false);
-//
-// const product = ref({});
-//
+import { ref, onMounted } from "vue";
+import { useQuasar } from "quasar";
+import { useRoute, useRouter } from "vue-router";
+import axios from "axios";
 import produtospecs from "src/components/produtospecs.vue";
 import produtosloading from "src/components/loadings/produtosloading.vue";
-//
-// async function searchProductById (productId) {
-//   try {
-//     product.value = await axios.get(`https://mitaoficial.elevarcommerceapi.com.br/HandoverMetasWS/webapi/handover/portal/servicoService/productBySlugEcommerceV2/$// {productId}/-1`).then(e => e.data);
-//   } catch (e) {
-//     $q.notify({
-//       message: "Erro ao buscar produto, redirecionando para página principal!"
-//     });
-//     router.push("/");
-//   }
-// }
-//
-// onMounted(async () => {
-//   await searchProductById(route.params.produto);
-// });
+
+const route = useRoute();
+const router = useRouter();
+const $q = useQuasar();
+const itsLoading = ref(false);
+const product = ref({});
+
+async function searchProductById (productId) {
+  try {
+    itsLoading.value = false;
+    product.value = await axios.get(`https://banbancalcados.elevarcommerceapi.com.br/HandoverMetasWS/webapi/handover/portal/servicoService/productBySlugEcommerceV2/${productId}/-1`).then(e => e.data);
+  } catch (e) {
+    $q.notify({
+      message: "Erro ao buscar produto, redirecionando para página principal!"
+    });
+    router.push("/");
+  } finally {
+    itsLoading.value = true;
+  }
+}
+
+onMounted(async () => {
+  await searchProductById(route.params.produto);
+});
+
 </script>
 
 <template lang="pug">
 q-page-container
-  template(
+  produtospecs(
     v-if="itsLoading"
+    :product="product"
   )
-    produtosloading
-  template(
-    v-if="!itsLoading"
+  produtosloading(
+    v-else
   )
-    produtospecs
 </template>
 
 <style scoped>
