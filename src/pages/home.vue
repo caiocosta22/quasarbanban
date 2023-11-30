43012af7b8e279e5f57ab07135ee72d7a71cbca4
caiocosta22 <caiocosta22@gmail.com>
2023-11-30 09:21:27 -0300

    saudação implementada

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -12,7 +12,14 @@ q-page-container
 </template>
 
 <script setup>
-// desktop
+import { onMounted } from "vue";
+import { useQuasar } from "quasar";
+
+const $q = useQuasar();
+const loginSalvo = $q.localStorage.getItem("login");
+const senhaSalva = $q.localStorage.getItem("senha");
+const nomeSalvo = $q.localStorage.getItem("nome");
+
 import bannerfull from "src/components/banners/bannerfull.vue";
 import bannerregua from "src/components/banners/bannerregua.vue";
 import carouselcategorias from "src/components/carousels/carouselcategorias.vue";
@@ -21,6 +28,17 @@ import bannermeio from "src/components/banners/bannermeio.vue";
 import bannermarca from "src/components/banners/bannermarca.vue";
 import carouselprodutos from "src/components/carousels/carouselprodutos.vue";
 import bannerinstagram from "src/components/banners/bannerinstagram.vue";
+
+onMounted(() => {
+  if (loginSalvo && senhaSalva) {
+    $q.notify({
+      color: "green-4",
+      textColor: "white",
+      icon: "check",
+      message: `Bem vindo, ${nomeSalvo}`
+    });
+  }
+});
 </script>
 
 <style scoped>
